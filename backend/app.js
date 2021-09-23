690f61d0a4c27af8e15e87e5a84ab1f2a7afb0e6
Hemant-Lohar <hemantlohar585@gmail.com>
2021-09-23 23:07:30 +0530

    created react frontend

--- backend/app.js
+++ backend/app.js
@@ -2,8 +2,49 @@ const express = require('express');
 const app = express();
 
 // DB Connection
+var mysql  = require('mysql');
+
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : '',
+  database : 'test'
+});
+ 
+connection.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected"); 
+});
+
+// var sql = "INSERT INTO mytable VALUES ('Hemant', 123456789)";
+
+//     connection.query(sql, function(err, result) {
+//         if(err) throw err;
+//         console.log("Inserted");
+//     });
+
+    app.get('/app',(req, res) => {
+        
+        connection.query("SELECT * FROM mytable", function(err, result) {
+        if(err) throw err;
+        console.log("Success");
+        
+        
+        
+        // if(result[0].name == "Hemnt")
+        // {console.log("Success")}
+        // else{
+        //     console.log("fail");
+        // }
+        console.log("error");
+        res.send("hi")
+
+    })
+})
 
 
+ 
+connection.end();
 
 // PORT
 const port = 5000;
